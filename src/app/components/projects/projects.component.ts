7a9ab091da739d304101b04ac35f9e855ca56de6
n4rk <elhcm99@gmail.com>
2024-03-31 22:21:43 +0000

    feat: tweaked project's models and services to feat our servless API

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {ProjectService} from "../../services/project.service";
+import {Project} from "../../models/project.model";
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  projects !: Observable<Array<Project>>;
+  constructor(private projectService:ProjectService){}
 
+  ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    this.projects = this.projectService.getProjects();
+  }
 }
